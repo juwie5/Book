81cf8445a19686f01b19e02ed48e73e8be23d774
unknown <bibire95@gmail.com>
2021-09-26 16:41:23 +0100

    Initial commit

---
+++ components/Book.vue
@@ -0,0 +1,42 @@
+<template>
+   <div class="container">
+      <div class="header">
+                  <div class="navbar-img">
+                      <img src="../assets/Vector.svg" alt="navimg">
+                 </div>
+                 <nav>
+                  <ul>
+                    <li><a href="#">Genres<span><img class="ul-img" src="../assets/arrow-down.svg" alt=""></span></a></li>
+                    <li><a href="#">Bestsellers</a></li>
+                    <li><a href="#">New Releases</a></li>
+                    <li><a href="#">Explore</a></li>
+                    <li><a href="#"> Sign In</a></li>
+                  </ul>
+                </nav>
+        </div>
+      <div class="main">
+        <h1>Endless knowledge</h1>
+        <h3>For all book readers and listeners, comfort.</h3>
+        <button class="btn">Start Reading <span><img src="../assets/book-open.svg" alt="book-svg"></span></button>
+        <h5>Only $8.99/month after free 30-day trial. Cancel anytime.</h5>
+      </div>
+      <div class="slide"></div>
+
+   </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Book',
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <Book/>
+</template>
+
+<script>
+export default {}
+</script>
+
+
+<style lang="scss">
+@import "styles/app"
+</style>
